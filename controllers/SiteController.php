3e5e4e85849a252d349ff0f9c8af9831df7d503f
Alexander-w-dev <yampi.ru@gmail.com>
2017-04-01 14:47:43 +0500

    fix actionStub

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -130,7 +130,7 @@ class SiteController extends Controller
      */
     public function actionContact()
     {
-        return actionStub();
+        return $this->actionStub();
         $model = new ContactForm();
         if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
             Yii::$app->session->setFlash('contactFormSubmitted');
@@ -149,7 +149,7 @@ class SiteController extends Controller
      */
     public function actionAbout()
     {
-        return actionStub();
+        return $this->actionStub();
         return $this->render('about');
     }
 
